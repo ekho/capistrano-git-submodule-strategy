0d2918965cd382d2fe18a42570ae8fd3deee5857
Boris Gorbylev <ekho@ekho.name>
2014-05-22 21:45:02 +0400

    first version

---
+++ capistrano-git-submodule-strategy.gemspec
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |gem|
+  gem.name          = "capistrano-git-submodule-strategy"
+  gem.version       = '0.1.0'
+  gem.authors       = ["Boris Gorbylev"]
+  gem.email         = ["ekho@ekho.name"]
+  gem.description   = %q{Git submodule support for Capistrano 3}
+  gem.summary       = %q{Git submodule support for Capistrano 3}
+  gem.homepage      = "http://github.com/i-ekho/capistrano-git-submodule-strategy"
+
+  gem.files         = `git ls-files`.split($/)
+  # no tests as of yet
+  # gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ["lib"]
+
+  gem.licenses      = ['MIT']
+
+  gem.add_dependency 'capistrano', '~> 3.1'
+end
---
+++ lib/capistrano/capistrano-git-submodule-strategy.rb
@@ -0,0 +1,33 @@
+require 'capistrano/git'
+
+class Capistrano::Git
+  module SubmoduleStrategy
+    # do all the things a normal capistrano git session would do
+    include Capistrano::Git::DefaultStrategy
+
+    def test
+      test! " [ -d #{repo_path}/.git ] "
+    end
+
+    def check
+      test! :git, :'ls-remote', repo_url
+    end
+
+    def clone
+      git :clone, '-b', fetch(:branch), '--recursive', repo_url, repo_path
+    end
+
+    def update
+      git :remote, :update
+    end
+
+    # put the working tree in a release-branch,
+    # make sure the submodules are up-to-date
+    # and copy everything to the release path
+    def release
+      git :checkout, fetch(:branch)
+      git :submodule, :update, '--init', '--recursive'
+      context.execute "rsync -ar --exclude=.git\* #{repo_path}/ #{release_path}"
+    end
+  end
+end
